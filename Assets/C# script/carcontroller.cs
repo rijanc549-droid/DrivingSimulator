63b6d2385c5ca891ea70ea32f5f3ebd811844baa
pradhansandesh007-maker <pradhansandesh007@gmail.com>
2026-05-14 12:38:01 +0545

    car and rigidbody

---
+++ Assets/C# script/carcontroller.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turnSpeed = 100f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, move);
+        transform.Rotate(0, turn, 0);
+    }
+}
